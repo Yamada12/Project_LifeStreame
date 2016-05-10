f55b421d95f0e0b6fe3b578e75e2cf3eeeb1c972
Taro Yamada <yamada.8616@gmail.com>
2016-05-10 15:24:01 +0900

    初期設定画面実装

--- Project_LifeStreame/Assets/Script/PlayerStatus.cs
+++ Project_LifeStreame/Assets/Script/PlayerStatus.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerStatus : MonoBehaviour
 {
-    static public bool mf;          //性別　true＝男性　false=女性
+    static public int mf;          //性別　0＝男性　1=女性
     static public string clanName;  //一族の名前
     static public float paramater1; //～の数値
     static public float paramater2; //～の数値
@@ -14,15 +14,16 @@ public class PlayerStatus : MonoBehaviour
     static public float lifeTime;   //残り時間
     static public int progress;     //進行度   小学校＝１　2年生＝２　etc...
     static public string job;       //ジョブ名（職業名）
-    static public bool marriage;    //結婚しているかどうか？　true＝結婚している　false=結婚してない
+    static public int marriage;    //結婚しているかどうか？　0＝結婚している　1=結婚してない
     static public int children;     //子供の人数（双子かどうか？）
     static public int schoolFlag;   //所属学校（０なら無所属、１なら？？学校　etc...）
+    static public int clubFlag;     //部活動は何系か？　0＝文化　1＝運動　2=無所属
     static public int courseYear;   //経過年数（現在の進行度に何年滞在したか？※学年など）
 
     // Use this for initialization
     static public void initStatus ()
     {//はじめから開始時にすべての変数をリセットするメソッド
-        mf = true;  //入力なしの場合は男性になる
+        mf = 0;  //入力なしの場合は男性になる
         clanName = "佐々木";   //入力なしの場合は佐々木になる
         paramater1 = 0;
         paramater2 = 0;
@@ -33,10 +34,17 @@ public class PlayerStatus : MonoBehaviour
         lifeTime = 180f;      //仮代入、一番初めに使う値をいれる
         progress = 0;
         job = "無職";         //暫定無職、小学生とかいれてもいいかも？
-        marriage = false;    //生まれながら結婚している奴などいない！
+        marriage = 0;    //生まれながら結婚している奴などいない！
         children = 1;        //残機的な扱いのつもり（０でゲームオーバー的な）
         schoolFlag = 0;      //0=無所属
+        clubFlag = 2;       //初期は無所属なので２
         courseYear = 1;      //経過年数なので初期値は１年目
+    }
+
+    public void DebugButton()
+    {//デバッグ用ボタン用
+        Debug.Log("ClanName is…" + clanName);
+        Debug.Log("mf is…" + mf);
     }
 
 }
--- Project_LifeStreame/Assets/Script/SaveSystem.cs
+++ Project_LifeStreame/Assets/Script/SaveSystem.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SaveSystem : MonoBehaviour {
+public class SaveSystem : MonoBehaviour
+{
+    static public void static_Save()
+    {
+        PlayerPrefs.SetString("clanName", PlayerStatus.clanName);
+        PlayerPrefs.SetInt("mf", PlayerStatus.mf);
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    static public void static_Load()
+    {
+        PlayerStatus.clanName = PlayerPrefs.GetString("clanName");
+        PlayerStatus.mf = PlayerPrefs.GetInt("mf");
+    }
+
+
 }
